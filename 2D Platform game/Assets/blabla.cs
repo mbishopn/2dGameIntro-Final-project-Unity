a9852c0d1edcb7760176146c92bf143176236648
DavidDgebuadze <75311974+DatoDgebuadze@users.noreply.github.com>
2024-01-22 14:30:27 -0500

    second test push

--- 2D Platform game/Assets/blabla.cs
+++ 2D Platform game/Assets/blabla.cs
@@ -2,13 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class wbabadfba : MonoBehaviour
+public class blabla : MonoBehaviour
 {
-    public GameObject bla;
-
-    public float spinx;
-
-    public float spiny;
     // Start is called before the first frame update
     void Start()
     {
