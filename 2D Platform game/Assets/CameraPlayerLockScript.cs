fa80a8e5eff5098434b8c1bd600f7ac9ec972d82
DavidDgebuadze <75311974+DatoDgebuadze@users.noreply.github.com>
2024-03-05 00:04:43 -0500

    PlayerLockOnCamera/LevelAdvancments

---
+++ 2D Platform game/Assets/CameraPlayerLockScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPlayerLockScript : MonoBehaviour
+{
+    // Setting up variables to control the speed of the the camera following the player
+    public float SpeedOfCamera = 2f;
+    // To get the position of the player
+    public Transform PlayerPos;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // to get players position xy
+        Vector3 newPos = new Vector3(PlayerPos.position.x, PlayerPos.position.y, -10f);
+        transform.position = Vector3.Slerp(transform.position, newPos, SpeedOfCamera*Time.deltaTime);
+    }
+}
