74dcac86f18accfe4893262c285cc9f2bfa7c83c
Vipul Sharma <vc@testemail.com>
2024-04-07 20:09:52 -0400

    level

---
+++ 2D Platform game/Assets/PlatformController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformController : MonoBehaviour
+{
+    public Transform posA, posB;
+    public int Speed;
+    Vector2 targetPos;
+    void Start()
+    {
+        targetPos = posB.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, posA.position) < .1f) targetPos = posB.position;
+
+        if (Vector2.Distance(transform.position, posB.position) < .1f) targetPos = posA.position;
+
+        transform.position = Vector2.MoveTowards(transform.position, targetPos, Speed * Time.deltaTime);
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+     if (collision.CompareTag("Player"))
+        {
+            collision.transform.SetParent(this.transform);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            collision.transform.SetParent(null);
+        }
+    }
+
+}
